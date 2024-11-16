b522cbdb9a16612a6096c93281d87b96e1f48328
M07aditya <2022pcecsaditya007@poornima.org>
2024-11-16 10:01:48 +0530

    pushing soulsync

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+import forms from '@tailwindcss/forms';
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    forms,
+  ],
+}
+
+
